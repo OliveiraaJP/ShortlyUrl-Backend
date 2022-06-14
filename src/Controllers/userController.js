5782eb9393592a7a34db9da2b469c0e4aa95c766
Joao Paulo <oliveirajpufrj@gmail.com>
2022-06-14 16:16:19 -0300

    feat: rank

--- src/Controllers/userController.js
+++ src/Controllers/userController.js
@@ -23,7 +23,24 @@ export const getUsersId = async (req, res) => {
     })
   } catch (error) {
     console.log('get user id error: ', error);
+    res.sendStatus(500);
   }
 };
 
-export const getRanking = async (req, res) => {};
+export const getRanking = async (req, res) => {
+    try {
+      const query = await db.query(
+        `SELECT u.id, u.name, COUNT(urls.id) as "linksCount", SUM(urls."countVisit") as "visitCount" 
+        FROM urls
+        JOIN users u ON urls."userId"=u.id
+        GROUP BY u.id
+        ORDER BY "visitCount" DESC
+        LIMIT 10`)
+        
+      const rank = query.rows;
+      res.send(rank).status(201)
+    } catch (error) {
+      console.log('get ranking error: ', error);
+      res.sendStatus(500);
+    }
+};
